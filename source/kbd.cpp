434a3ed6e07a34d6935885baaba9ef1ae538efac
Pk11 <epicpkmn11@outlook.com>
2022-03-16 21:11:23 -0500

    Make keys a bit larger

--- source/kbd.cpp
+++ source/kbd.cpp
@@ -9,17 +9,19 @@
 #include <nds.h>
 
 Kbd::Kbd() {
-	constexpr int tileSize = 16 * 16 / 2;
+	constexpr int tileSize = 32 * 32 / 2;
 	for(int i = 0; i < kbdKeysTilesLen / tileSize; i++) {
-		_gfx.push_back(oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_16Color));
+		_gfx.push_back(oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_16Color));
 		tonccpy(_gfx.back(), kbdKeysTiles + (i * tileSize), tileSize);
 	}
-	_backspaceGfx = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_16Color);
+
+	_backspaceGfx = oamAllocateGfx(&oamSub, SpriteSize_64x32, SpriteColorFormat_16Color);
 	tonccpy(_backspaceGfx, backspaceKeyTiles, backspaceKeyTilesLen);
-	_enterGfx = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_16Color);
+	_enterGfx = oamAllocateGfx(&oamSub, SpriteSize_64x32, SpriteColorFormat_16Color);
 	tonccpy(_enterGfx, enterKeyTiles, enterKeyTilesLen);
+
 	for(Key &key : _keys) {
-		_sprites.emplace_back(false, (key.c == u'\b' || key.c == u'\n') ? SpriteSize_32x32 : SpriteSize_16x16, SpriteColorFormat_16Color);
+		_sprites.emplace_back(false, (key.c == u'\b' || key.c == u'\n') ? SpriteSize_64x32 : SpriteSize_32x32, SpriteColorFormat_16Color);
 		_sprites.back().move(key.x, key.y).gfx(key.c == u'\b' ? _backspaceGfx : (key.c == u'\n' ? _enterGfx : _gfx[letterIndex(key.c)])).visible(false);
 	}
 	Sprite::update(false);
@@ -33,8 +35,8 @@ char16_t Kbd::get() {
 	touchRead(&touch);
 
 	for(const Key &key : _keys) {
-		int size = (key.c == u'\b' || key.c == u'\n') ? 32 : 16;
-		if(touch.px >= key.x && touch.px < key.x + size && touch.py >= key.y && touch.py < key.y + size)
+		int size = (key.c == u'\b' || key.c == u'\n') ? 35 : 23;
+		if(touch.px >= key.x && touch.px < key.x + size && touch.py >= key.y && touch.py < key.y + 23)
 			return key.c;
 	}
 

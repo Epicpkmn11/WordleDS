2c359032ed25b32bc7cd6462452becedf716f842
Pk11 <epicpkmn11@outlook.com>
2022-11-08 23:24:07 -0600

    Fix word scraper, update word list
    Turns out they actually did make a change, they've switched to an API to get answers instead of having them all in the code

--- resources/update-words.py
+++ resources/update-words.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser, FileType
+from datetime import datetime, timezone
 from requests import get
 from bs4 import BeautifulSoup
 
 import json
 import re
 
+
+def scrape_answers(start):
+	"""Scrapes the upcoming answers"""
+
+	day = start
+	words = []
+
+	while True:
+		date = datetime.fromtimestamp(1624078800 + day * 60 * 60 * 24, timezone.utc)
+		day += 1
+		json = get(date.strftime("https://www.nytimes.com/svc/wordle/v2/%Y-%m-%d.json")).json()
+		if "status" in json:
+			break
+
+		print("%(days_since_launch)d (%(print_date)s) - %(editor)s" % json)
+		words.append(json["solution"])
+
+	return words
+
+
 def update_words(output):
+	"""Updates the word lists"""
+
+	# First get the HTML and figure out what the current JS is
 	html = get("https://www.nytimes.com/games/wordle/index.html").text
 	soup = BeautifulSoup(html, "html.parser")
 	script, = [script["src"] for script in soup.findAll("script") if script.has_attr("src") and script["src"].startswith("https://www.nytimes.com/games-assets/v2/wordle.")]
 
+	# So we can get that and grab the base list incase they ever update that again
 	js = get(script).text
-	guesses = json.loads(re.findall(r'\["aahed"(?:,"\w{5}")*(?=,"cigar")', js)[0] + "]")
+	guesses = json.loads(re.findall(r'\["aahed"(?:,"\w{5}")*\]', js)[0])
+	guesses.sort()
 	choices = json.loads("[" + re.findall(r'"cigar"(?:,"\w{5}")*\]', js)[0])
+
+	# Now scrape the real upcoming answers since that's in a special API now
+	choices = choices[:507] + scrape_answers(506)  # 506 is the day they started this
 
 	output.write('''#include "words.hpp"
 
@@ -30,6 +59,7 @@ std::vector<std::u16string> Words::guesses = {u"''')
 	output.write('", u"'.join([word.upper() for word in guesses]))
 
 	output.write('"};\n')
+
 
 if __name__ == "__main__":
 	parser = ArgumentParser(description="Updates the Wordle DS word list")

1cdccdce97bf6acc6090b12e658f1746ae0d2ffc
Pk11 <pk11@outlook.jp>
2023-02-28 16:51:14 +0000

    Fix errors

--- source/wifi.cpp
+++ source/wifi.cpp
@@ -6,41 +6,42 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <string>
+#include <string.h>
 #include <sys/socket.h>
 
 std::string httpGet(const char *host, const char *path) {
-	const char request[256];
+	char request[256];
 	snprintf(request, sizeof(request), "GET %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: Wordle DS\r\n\r\n", path, host);
 
-	hostent *host = gethostbyname(host);
-	int socket = socket(AF_INET, SOCK_STREAM, 0);
+	hostent *hostEnt = gethostbyname(host);
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
 
 	sockaddr_in sain;
 	sain.sin_family = AF_INET;
 	sain.sin_port = htons(80);
-	sain.sin_addr.s_addr = *(u32 *)(host->h_addr_list[0]);
-	connect(socket, (sockaddr *)&sain, sizeof(sain));
+	sain.sin_addr.s_addr = *(u32 *)(hostEnt->h_addr_list[0]);
+	connect(sock, (sockaddr *)&sain, sizeof(sain));
 
-	send(socket, request, strlen(request), 0);
+	send(sock, request, strlen(request), 0);
 
 	int len;
 	char buffer[256];
 	std::string response;
 
-	while((len = recv(socket, buffer, 255, 0)) != 0) {
+	while((len = recv(sock, buffer, 255, 0)) != 0) {
 		buffer[len] = '\0';
 		response += buffer;
 	}
 
-	shutdown(socket, 0);
-	closesocket(socket);
+	shutdown(sock, 0);
+	closesocket(sock);
 
 	return response;
 }
 
 void getWords(int day) {
 	if(!Wifi_CheckInit()) {
-		Wifi_InitDefault();
+		Wifi_InitDefault(WFC_CONNECT);
 	} else {
 		Wifi_EnableWifi();
 	}
